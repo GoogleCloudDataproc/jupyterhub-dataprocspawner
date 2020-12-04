d5e4e5edcc78965a68bdc4fd37eebaab4667d41a
Matthieu Mayran <mayran@google.com>
2020-12-04 18:06:00 +0000

    Update spawner server check for DataprocHubServer and Server

--- dataprochub/app.py
+++ dataprochub/app.py
@@ -13,8 +13,10 @@
 # limitations under the License.
 
 """ Replaces default JupyterHub.app to handle redirects. """
+
 import re
 
+from dataprocspawner.spawnable import DataprocHubServer
 from jupyterhub.app import JupyterHub
 from jupyterhub.handlers.base import UserUrlHandler
 from jupyterhub.objects import Server
@@ -27,14 +29,18 @@ class DataprocHubUserUrlHandler(UserUrlHandler):
     self.statsd.incr('redirects.user_after_login')
     redirect_url = None
     tmp_spawner = user.spawners[spawner.name]
-    tmp_spawner_server = tmp_spawner._server
+    tmp_spawner_server = tmp_spawner.server
     self.log.debug(f'# spawners are {user.spawners}')
-    self.log.info(f'# spawner._server value is {tmp_spawner_server}')
-    if type(tmp_spawner_server) == Server:
+    self.log.info(f'# spawner.server value is {tmp_spawner_server}')
+    if isinstance(tmp_spawner_server, DataprocHubServer):
+      self.log.info('# Read component gateway url from DataprocHubServer.')
+      redirect_url = tmp_spawner.component_gateway_url
+    elif isinstance(tmp_spawner_server, Server):
+      self.log.info('# Make component gateway url from Server url.')
       url_parts = re.search('(.+?)://(.+?)/(.+?)', tmp_spawner_server.url)
       redirect_url = f'{url_parts.group(1)}://{url_parts.group(2)}/jupyter/lab'
     else:
-      redirect_url = tmp_spawner.component_gateway_url
+      self.log.info('# Can not type identify spawner server.')
 
     self.log.info(f'# Redirecting to notebook at {redirect_url}.')
     self.redirect(redirect_url)
